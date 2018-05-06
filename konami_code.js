b188b1ad4d3798292052092f7a3c8d620ca4e899
Eddie Melo <heriberto@melo.nyc>
2018-05-06 21:18:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  function onKeyDownHandler(e){
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]){
