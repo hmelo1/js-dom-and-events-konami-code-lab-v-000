35f8a5fa626550fe44cf1675b2c15a8d1dc66ff6
Eddie Melo <heriberto@melo.nyc>
2018-05-06 21:16:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,6 +6,18 @@ function init() {
   let index = 0;
 
   function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which);
 
+    if (key === code[index]){
+      index++;
+
+      if (index === 2){
+        alert("test");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   }
 }
