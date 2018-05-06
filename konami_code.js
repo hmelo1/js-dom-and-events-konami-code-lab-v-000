5fdf1d5a0a4d9fbd047dc683c08a35ae709feb57
Eddie Melo <heriberto@melo.nyc>
2018-05-06 21:21:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
-// Key codes for A, B, and C keys.
-const alphabet = [65, 66, 67];
+function init() {
+  // Write your JavaScript code inside the init() function
+  let index = 0;
 
-// Keep track of index outside of the event handler.
-let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
+    if (key === code[index]){
+      index++;
 
-  if (key === alphabet[index]) {
-    index++;
+      if (index === 2){
+        alert("test");
 
-    if (index === alphabet.length) {
-      alert("Hurray!");
-
+        index = 0;
+      }
+    } else {
+      alert("wrong!")
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+  })
 }
